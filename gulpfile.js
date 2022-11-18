872b8e078964195f549c9bc5590649e0b3ce2fe4
Stepan04 <114218948+Stepan04@users.noreply.github.com>
2022-11-18 10:51:28 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ global.app = {
 
 
 
-import {copy} from "./gulp/tasks/copy.js";
+import {copy,copycss} from "./gulp/tasks/copy.js";
 import {reset} from "./gulp/tasks/reset.js";
 import {html} from "./gulp/tasks/html.js";
 import {server} from "./gulp/tasks/server.js";
@@ -25,12 +25,13 @@ import {images} from "./gulp/tasks/images.js";
 
 function watcher(){
     gulp.watch(path.watch.files, copy);
+   // gulp.watch(path.watch.files, copycss);
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
     gulp.watch(path.watch.js, js);
     gulp.watch(path.watch. images, images);
 }
-const mainTasks = gulp.parallel(copy, html, scss, js, images);
+const mainTasks = gulp.parallel(copy, html, scss, copycss, js, images);
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher,server));
 
